52dbd3faf7976e4caadbbba96d7dccbf30801fa2
Bea <beatrizalvarezmoreno@gmail.com>
2018-05-10 10:36:41 +0200

    routes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import { routes } from './routes.js'
 import App from './App.vue'
+
+
+Vue.use(VueRouter);
+//array de routes[con S]-path es una clave del objeto--component en singular.
+
+//Ahora aauí se le pasan las rutas. En español, enrutador.
+const router = new VueRouter({
+	routes,
+	mode : 'history'
+})
 
 new Vue({
   el: '#app',

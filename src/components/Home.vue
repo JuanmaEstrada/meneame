f1416408d22fb348a1599cad188e4e0bc89b6ec4
Cayetano <cayetano92@hotmail.com>
2018-05-10 13:44:04 +0200

    home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,39 @@
 <template>
-<h1>Home component</h1>
+	<div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-12 col-md-12">
+        <h3>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, laudantium!
+        </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, rerum iure ab, laboriosam delectus maiores eaque perspiciatis fuga consequuntur sint deserunt saepe? Repudiandae, eaque ipsum totam rem. Velit, modi, minus.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-2 col-md-2">
+        <span>
+          52 <br> <a href="">script-voto</a> <br> botos
+        </span>
+      </div>
+      <div class="col-sm-8 col-md-8">
+        <h2>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, laudantium!
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, rerum iure ab, laboriosam delectus maiores eaque perspiciatis fuga consequuntur sint deserunt saepe? Repudiandae, eaque ipsum totam rem. Velit, modi, minus.
+        </p>
+        <ul class="list-inline">
+          <li class="list-inline-item"><a href="#">N Comentarios</a></li>
+          <li class="list-inline-item"><a href="#">comporatir</a></li>
+          <li class="list-inline-item"><a href="#">votos positivos</a></li>
+          <li class="list-inline-item"><a href="#">votos negativos</a></li>
+          <li class="list-inline-item"><a href="#">actualidad</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-2 col-md-2">
+       <img class="img-article" src="#" alt="155*155">
+      </div>
+    </div>
+  </div>
 </template>

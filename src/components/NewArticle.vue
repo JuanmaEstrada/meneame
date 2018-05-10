e090c7c60c84991e3195697ffcd4baa9eb870b35
ElyCruzGuzman <elycruzguzman7@hotmail.com>
2018-05-10 18:19:31 +0200

    newArticle

--- src/components/NewArticle.vue
+++ src/components/NewArticle.vue
@@ -9,7 +9,7 @@
 		<div class="form-group">
 		  <label class="col-md-4 control-label" for="textinput">Titulo</label>  
 		  <div class="col-md-4">
-		  <input id="textinput" name="textinput" type="text" class="form-control input-md" required="">
+		  <input id="titulo" name="titulo" type="text" class="form-control input-md" required="" v-model="titulo">
 		    
 		  </div>
 		</div>
@@ -18,7 +18,7 @@
 		<div class="form-group">
 		  <label class="col-md-4 control-label" for="textinput">Entradilla</label>  
 		  <div class="col-md-4">
-		  <input id="textinput" name="textinput" type="text" class="form-control input-md" required="">
+		  <input id="entradilla" name="entradilla" type="text" class="form-control input-md" required="" v-model="entradilla">
 		    
 		  </div>
 		</div>
@@ -27,7 +27,7 @@
 		<div class="form-group">
 		  <label class="col-md-4 control-label" for="textinput">Categoria</label>  
 		  <div class="col-md-4">
-		  <input id="email" name="textinput" type="text" class="form-control input-md" required="">
+		  <input id="categoria" name="categoria" type="text" class="form-control input-md" required="" v-model="categoria">
 		    
 		  </div>
 		</div>
@@ -36,7 +36,7 @@
 		<div class="form-group">
 		  <label class="col-md-4 control-label" for="textinput">Url del Articulo</label>
 		  <div class="col-md-4">
-		    <input id="textinput" name="textinput" type="text" class="form-control input-md" required="">
+		    <input id="url" name="url" type="text" class="form-control input-md" required="" v-model="url">
 		    
 		  </div>
 		</div>
@@ -45,10 +45,40 @@
 		<div class="form-group">
 		  <label class="col-md-4 control-label" for="singlebutton"></label>
 		  <div class="col-md-4">
-		    <button id="singlebutton" name="singlebutton" class="btn btn-primary">Publicar</button>
+		    <button id="singlebutton" name="singlebutton" class="btn btn-primary" @click="sendArticle">Publicar</button>
 		  </div>
 		</div>
 
 		</fieldset>
 	</form>
 </template>
+
+<script>
+	export default {
+  data(){
+    return {
+    	titulo:"",
+    	entradilla: "",
+    	categoria: "",
+    	url: ""
+    }
+  },
+  methods: {
+  	sendArticle: function(e){
+  		e.preventDefault()
+  		fetch('http://localhost:3000/api/articles', {
+				method: 'post',
+				 headers: {
+    'Content-Type': 'application/json'
+  },
+				body: JSON.stringify({
+					titulo: this.titulo,
+					entradilla: this.entradilla,
+					categoria: this.categoria,
+					url: this.url
+			})
+	});
+  	}
+  }
+}
+</script>

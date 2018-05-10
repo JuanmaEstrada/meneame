806ac68b276d4e4e4285b89632440d48fc428f1d
Bea <beatrizalvarezmoreno@gmail.com>
2018-05-10 13:59:27 +0200

    routes

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,16 +1,11 @@
 <template>
 <div class="row header-second align-items-center">
-
-						
-
 							<div class="col-md-5 user-tools form-group m-3">
-
 							<ul class="nav ml-3">
-								<li clas="nav-item mr-1"><button class="btn" type="button"> +PUBLICAR</button></li>
-								<li clas="nav-item"><button class="btn" type="button"> +CREAR ARTICULO</button></li>
+								<button class="btn" type="button"><router-link :to="{name :'new-articleLink'}" tag="li" class="nav-item"> +CREAR ARTICULO </router-link></button>
 								<li clas="nav-item"><button class="btn" type="button"> CATALOGO </button></li>
+								<li clas="nav-item"><button class="btn" type="button"> NOTICIAS </button></li>
 							</ul>
-
 						</div>
 					</div>
 </template>
